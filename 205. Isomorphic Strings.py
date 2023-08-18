43f6948445bdaa5b3fca6c1543ca2358e386dc63
Rahul <rahulkrish321123@gmail.com>
2023-08-19 02:44:08 +0530

    15

--- 205. Isomorphic Strings.py
+++ 205. Isomorphic Strings.py
@@ -1,6 +1,7 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         iso = {}
+        a = []
         if len(s)!=len(t):
             return False
         for i in range(len(s)):
@@ -9,6 +10,9 @@ class Solution:
                     continue
                 else:
                     return False
+            elif t[i] in a:
+                return False
             else:
                 iso[s[i]]=t[i]
+                a.append(t[i])
         return True
